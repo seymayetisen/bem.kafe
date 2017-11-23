3c6eb690786e0fd51103f0ad878f6276361fe3e1
Orhan Aygün <orhan.aygun@outlook.com>
2017-11-23 11:32:09 +0300

    - Sayfalama stroed prosedürleri eklendi

---
+++ SayfaSayisinaGoreCalisanGetir.sql
@@ -0,0 +1,23 @@
+﻿USE [KafeYonetim]
+GO
+/****** Object:  StoredProcedure [dbo].[SayfaSayisinaGoreCalisanGetir]    Script Date: 23.11.2017 11:31:21 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[SayfaSayisinaGoreCalisanGetir] 
+	@sayfaNumarasi int,
+	@sayfadakiKayitSayisi int
+AS
+BEGIN
+	SELECT Calisan.*, CalisanGorev.GorevAdi FROM Calisan
+	INNER JOIN CalisanGorev ON Calisan.GorevId = CalisanGorev.Id
+ORDER BY Calisan.Id
+OFFSET ((@sayfaNumarasi -1) * @sayfadakiKayitSayisi) ROWS
+FETCH NEXT @sayfadakiKayitSayisi ROWS ONLY
+END
